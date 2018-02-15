f30ec95eb699202863fa68acc660fc2e4b1d844c
fabi <fmikulasch@googlemail.com>
2018-02-15 15:57:37 +0100

    ga loop started

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import random
 from filehandler import get_problem
 
 problem = "example.in"
@@ -36,3 +37,40 @@ def mutate(chrom, rows, cols, sigma):
 def find_elite(chroms, fitnesses, k):
     elites = fitnesses.argsort()[:k]
     return chroms[elites]
+
+def gen_population(rows, cols, min_rectangles, max_rectangles):
+    population = []
+    for i in population_size:
+        n = int(random() * (max_rectangles - min_rectangles) + min_rectangles)
+        chrom = to_chromosome(create_rectangles(n, rows, cols))
+        population.append(chrom)
+    return population
+
+def ga_loop(pizza, n_generations=100, population_size=10, n_elite=1,
+            crossover_prob=0.1, mutation_prob=0.5):
+    
+    rows, cols = pizza.shape
+    
+    min_rectangles, max_rectangles = get_opt_number_rectangles()
+    population = gen_population(rows, cols, min_rectangles, max_rectangles)
+    fitnesses = np.zeros(n_generations, population_size)
+
+    for gen in n_generations:
+        for i in range(population_size):
+            fitnesses[gen,i] = get_fitness(chrom, pizza)
+        
+        new_population = find_elite(population, fitnesses[gen,:], n_elite)
+        
+        for i in range(population_size - n_elite):
+            if random() < crossover_prob:
+                parent1, parent2 = select(population,fitnesses[gen,:],2)
+                child = crossover(parent1, parent2)
+            else:
+                child = select(population,fitnesses[gen,:],1)
+            if random() < mutation_prob:
+                child = mutate(child, rows, cols, sigma)
+            new_population.append(child)
+            
+        population = new_population
+    
+            
